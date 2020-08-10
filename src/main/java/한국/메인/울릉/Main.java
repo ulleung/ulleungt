21be960ff836f4a15a86f4fe5fd45733fafc5c9e
Janghyub Seo <seojanghyeob@gmail.com>
2020-08-10 10:15:26 +0900

    코딩 스타일 통일

--- src/main/java/한국/메인/울릉/Main.java
+++ src/main/java/한국/메인/울릉/Main.java
@@ -1,8 +1,7 @@
 package 한국.메인.울릉;
 
 public class Main {
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         System.out.println("Hello, World!");
     }
 }
