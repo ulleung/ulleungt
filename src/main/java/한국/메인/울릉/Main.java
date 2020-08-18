a05ef51f7714cd1ff9861928074696a45bb73e89
Janghyub Seo <seojanghyeob@gmail.com>
2020-08-18 18:06:08 +0900

    [수정] Main.java: Antlr 적용
    ParseTreeWalker 사용

--- src/main/java/한국/메인/울릉/Main.java
+++ src/main/java/한국/메인/울릉/Main.java
@@ -1,7 +1,43 @@
 package 한국.메인.울릉;
 
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import 한국.메인.울릉.Antlr.ulleungLexer;
+import 한국.메인.울릉.Antlr.ulleungParser;
+import 한국.메인.울릉.리스너.울릉리스너;
+
+import java.io.*;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Hello, World!");
+
+        String filename = args[0];
+
+        File inputUlnFile = new File(filename);
+        File outputJavaFile = new File(filename+".java");
+
+        try{
+            FileOutputStream fos = new FileOutputStream(outputJavaFile);
+            FileInputStream fis = new FileInputStream(inputUlnFile);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis, "UTF-8"));
+
+            String code = br.lines().collect(Collectors.joining());
+
+            //CharStream input = new CharStream(new FileInputStream(args[0]));
+
+            CharStream input = CharStreams.fromString(code);
+
+
+            ulleungLexer lexer = new ulleungLexer(input);
+            ulleungParser parser = new ulleungParser(new CommonTokenStream(lexer));
+            ParseTreeWalker.DEFAULT.walk(new 울릉리스너(fos), parser.program());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
