e206c5c55d33c5f85f0463789a59c8de53edafc0
Janghyub Seo <seojanghyeob@gmail.com>
2020-08-20 09:28:04 +0900

    [수정] Dok.kt: ulleung.g4의 수정 호환, 메서드 및 변수 번역 추가

--- src/main/java/한국/메인/울릉/Dok.kt
+++ src/main/java/한국/메인/울릉/Dok.kt
@@ -5,8 +5,8 @@ import 한국.메인.울릉.Antlr.ulleungParser
 class Dok {
 
     var classList = ArrayList<Pair<String, String>>() // 번역본 클래스명 / 실제 클래스명
-    var methodList = ArrayList<Pair<String, String>>() // 번역본 메서드명 / 실제 메서드명
-    var varList = ArrayList<Pair<String, String>>() //번역본 변수명 / 실제 변수명
+    var methodList = ArrayList<Triple<String, String, String>>() // 번역본 메서드명 / 번역본 클래스명 / 실제 메서드명
+    var varList = ArrayList<Pair<String, String>>() //번역본 변수명 / 번역본 클래스명 / 실제 변수명
 
     init {
         // TODO: 외부 .dok 파일 읽어오기
@@ -24,31 +24,118 @@ class Dok {
         classList.add(Pair("문자열", "String")) //Java 원시형은 아니지만 public static main(String args[]) 에서 사용되는 관계로 추가
     }
 
-    public fun packageNameContextToOriginalName(packageNamecontext: ulleungParser.Package_nameContext?) : String
-    {
+    fun typeNameContextToClassName(ctx: ulleungParser.Type_nameContext?) : String {
+        var returnStringBuilder = StringBuilder()
+
+        if(ctx?.package_name() != null)
+            returnStringBuilder.append(ctx?.package_name()?.text)
+
+        if(ctx?.package_name() != null && ctx?.IDENTIFIER() != null)
+            returnStringBuilder.append(".")
+
+        if(ctx?.IDENTIFIER() != null)
+            returnStringBuilder.append(stringToClassName(ctx?.IDENTIFIER()?.text!!))
+
+        return returnStringBuilder.toString()
+    }
+
+    fun typeNameArrayContextToClassName(ctx: ulleungParser.Type_name_arrayContext?) : String {
+        var returnStringBuilder = StringBuilder()
+
+        if(ctx?.package_name() != null)
+            returnStringBuilder.append(ctx?.package_name()?.text)
+
+        if(ctx?.package_name() != null && ctx?.IDENTIFIER() != null)
+            returnStringBuilder.append(".")
+
+        if(ctx?.IDENTIFIER() != null)
+            returnStringBuilder.append(stringToClassName(ctx?.IDENTIFIER()?.text!!))
+
+        if(ctx?.ARRAY() != null)
+            returnStringBuilder.append("[]")
+
+        return returnStringBuilder.toString()
+    }
+
+    fun classNameFromPackage(packageName: String) : String {
+        return packageName.split(".").last()
+    }
+
+    fun stringToClassName(className: String) : String {
         var returnString = ""
 
         var neededList = ArrayList<Pair<String, String>>()
 
-        for(pair in classList)
-        {
-            if(pair.first == packageNamecontext?.text)
+        for(pair in classList) {
+            if(pair.first == className)
                 neededList.add(pair)
         }
 
-        if(neededList.size > 1)
-        {
-            System.out.println("Dok 경고: %s 가 번역명인 객체가 너무 많습니다. 첫번째 값인 %s 로 진행합니다.".format(packageNamecontext?.text, neededList.get(0).second))
+        if(neededList.size > 1) {
+            System.out.println("Dok 경고: %s 이(가) 번역명인 객체가 너무 많습니다. 첫번째 값인 %s (으)로 진행합니다.".format(className, neededList.get(0).second))
         }
 
-        if(neededList.size == 0)
-        {
-            System.out.println("Dok 경고: %s 가 번역명인 객체가 존재하지 않습니다. 원본 이름으로 계속합니다.".format(packageNamecontext?.text))
-            return packageNamecontext?.text!!
+        if(neededList.size == 0) {
+            System.out.println("Dok 경고: %s 이(가) 번역명인 객체가 존재하지 않습니다. 원본 이름으로 계속합니다.".format(className))
+            return className
         }
 
         returnString = neededList.get(0).second
 
         return returnString;
+    }
+
+    fun stringToMethodName(methodName: String, className: String) : String {
+        var returnString = ""
+
+        var neededList = ArrayList<Triple<String, String, String>>()
+
+        if(className == "this")
+            return "";
+
+        for(pair in methodList) {
+            if(pair.first == methodName && pair.second == className)
+                neededList.add(pair)
+        }
+
+        if(neededList.size > 1) {
+            System.out.println("Dok 경고 : %s 에 %s 이(가) 번역명인 함수가 너무 많습니다. 첫번째 값인 %s (으)로 계속합니다.".format(className, methodName, neededList.get(0).third))
+        }
+
+        if(neededList.size == 0) {
+            System.out.println("Dok 경고: %s 에 %s 이(가) 번역명인 함수가 존재하지 않습니다. 원본 이름으로 계속합니다.".format(className, methodName))
+            return methodName
+        }
+
+        returnString = neededList.get(0).third
+
+        return returnString
+    }
+
+    fun stringToVarName(varName: String, className: String) : String {
+        var returnString = ""
+
+        var neededList = ArrayList<Triple<String, String, String>>()
+
+        if(className == "this")
+            return "";
+
+        for(pair in methodList) {
+            if(pair.first == varName && pair.second == className)
+                neededList.add(pair)
+        }
+
+        if(neededList.size > 1) {
+            System.out.println("Dok 경고 : %s 에 %s 이(가) 번역명인 변수가 너무 많습니다. 첫번째 값인 %s (으)로 계속합니다.".format(className, varName, neededList.get(0).third))
+        }
+
+        if(neededList.size == 0) {
+            System.out.println("Dok 경고: %s 에 %s 이(가) 번역명인 변수가 존재하지 않습니다. 원본 이름으로 계속합니다.".format(className, varName))
+            return varName
+        }
+
+        returnString = neededList.get(0).third
+
+        return returnString
     }
 }
